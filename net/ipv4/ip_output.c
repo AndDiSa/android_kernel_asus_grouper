LineageOS/android_kernel_samsung_exynos5420

am: udp: consistently apply ufo or fragmentation
Fixes: commit daca9eae5d24cd0d61cff1722e91eca2fac23a72
    "udp: consistently apply ufo or fragmentation"

Willy Tarreau pointed out that 3.10 (and older versions) backport
should use skb_has_frags() instead of skb_is_gso() because there
are two commits missing:

  "ip_output: do skb ufo init for peeked non ufo skb as well"
  (e93b7d748be887cd7639b113ba7d7ef792a7efb9)

  "ip6_output: do skb ufo init for peeked non ufo skb as well"
  (c547dbf55d5f8cf615ccc0e7265e98db27d3fb8b)

[basilgello: Thanks to syphyr for the heads-up]

Signed-Off-By: Vasyl Gello <vasek.gello@gmail.com>

Change-Id: I17935bbef7a7b283171118406f290333991b83d0

net/ipv4/ip_output.c
@@ -846,7 +846,7 @@ static int __ip_append_data(struct sock *sk,
 		csummode = CHECKSUM_PARTIAL;
 
 	cork->length += length;
-	if ((skb && skb_is_gso(skb)) ||
+	if ((skb && skb_has_frags(skb)) ||
 	    (((length + (skb ? skb->len : fragheaderlen)) > mtu) &&
 	    (skb_queue_len(queue) <= 1) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&

net/ipv4/udp.c
@@ -732,7 +732,7 @@ static int udp_send_skb(struct sk_buff *skb, struct flowi4 *fl4)
 	if (is_udplite)  				 /*     UDP-Lite      */
 		csum = udplite_csum(skb);
 
-	else if (sk->sk_no_check_tx && !skb_is_gso(skb)) {   /* UDP csum off */
+	else if (sk->sk_no_check_tx && !skb_has_frags(skb)) {   /* UDP csum off */
 
 		skb->ip_summed = CHECKSUM_NONE;
 		goto send;

net/ipv6/ip6_output.c
@@ -1342,10 +1342,12 @@ int ip6_append_data(struct sock *sk, int getfrag(void *from, char *to,
 
 	skb = skb_peek_tail(&sk->sk_write_queue);
 	cork->length += length;
-	if (((length > mtu) ||
-	     (skb && skb_has_frags(skb))) &&
+	if ((skb && skb_has_frags(skb)) ||
+	    (((length + fragheaderlen) > mtu) &&
+	    (skb_queue_len(&sk->sk_write_queue) <= 1) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
-	    (rt->dst.dev->features & NETIF_F_UFO)) {
+	    (rt->dst.dev->features & NETIF_F_UFO) &&
+	    (sk->sk_type == SOCK_DGRAM))) {
 		err = ip6_ufo_append_data(sk, getfrag, from, length,
 					  hh_len, fragheaderlen,
 					  transhdrlen, mtu, flags, rt);

AndDiSa/android_kernel_asus_grouper

backported from:

net/ipv4/ip_output.c
@@ -838,7 +838,7 @@ static int __ip_append_data(struct sock *sk,
 		csummode = CHECKSUM_PARTIAL;
 
 	cork->length += length;
-	if ((skb && skb_is_gso(skb)) ||
+	if ((skb && skb_has_frag_list(skb)) ||
 	    ((length > mtu) &&
 	    (skb_queue_len(queue) <= 1) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&

net/ipv4/udp.c
@@ -729,7 +729,8 @@ static int udp_send_skb(struct sk_buff *skb, struct flowi4 *fl4)
 
 	if (is_udplite)  				 /*     UDP-Lite      */
 		csum = udplite_csum(skb);
-	else if (sk->sk_no_check && !skb_is_gso(skb)) {   /* UDP csum off */
+
+	else if (sk->sk_no_check && !skb_has_frag_list(skb)) {   /* UDP csum off */
 		skb->ip_summed = CHECKSUM_NONE;
 		goto send;
 

net/ipv6/ip6_output.c
@@ -1322,10 +1322,12 @@ int ip6_append_data(struct sock *sk, int getfrag(void *from, char *to,
 
         skb = skb_peek_tail(&sk->sk_write_queue);
 	cork->length += length;
-        if (((length > mtu) ||
-             (skb && skb_is_gso(skb))) &&
+	if ((skb && skb_has_frag_list(skb)) ||
+	    (((length + fragheaderlen) > mtu) &&
+	    (skb_queue_len(&sk->sk_write_queue) <= 1) &&
             (sk->sk_protocol == IPPROTO_UDP) &&
-            (rt->dst.dev->features & NETIF_F_UFO)) {
+	    (rt->dst.dev->features & NETIF_F_UFO) &&
+	    (sk->sk_type == SOCK_DGRAM))) {
                 err = ip6_ufo_append_data(sk, getfrag, from, length,
                                           hh_len, fragheaderlen,
                                           transhdrlen, mtu, flags, rt);