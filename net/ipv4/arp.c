d5aea24b589c0ad51f8e80aa6fa3095e7f83e7ec
AndDiSa <anddisa@googlemail.com>
2025-04-09 16:28:24 +0200

    CVE-2024-26733

--- net/ipv4/arp.c
+++ net/ipv4/arp.c
@@ -1098,7 +1098,8 @@ static int arp_req_get(struct arpreq *r, struct net_device *dev)
 	neigh = neigh_lookup(&arp_tbl, &ip, dev);
 	if (neigh) {
 		read_lock_bh(&neigh->lock);
-		memcpy(r->arp_ha.sa_data, neigh->ha, dev->addr_len);
+		memcpy(r->arp_ha.sa_data, neigh->ha,
+		       min(dev->addr_len, (unsigned char)sizeof(r->arp_ha.sa_data)));
 		r->arp_flags = arp_state_to_flags(neigh);
 		read_unlock_bh(&neigh->lock);
 		r->arp_ha.sa_family = dev->type;
